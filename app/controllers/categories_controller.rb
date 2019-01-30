fc3f902b4c05bdc1b52d6b9858a50066ad2a2997
Samuel Honorato <samuel.honorato.silva@gmail.com>
2019-01-30 17:51:25 -0300

    Rotas de visualizacao e criacao de Categoria. Gem twitter e internacionalizacao

---
+++ app/controllers/categories_controller.rb
@@ -0,0 +1,19 @@
+class CategoriesController < ApplicationController
+
+  def index
+    @categories_all = Category.order(created_at: :desc)
+    @category_new = Category.new
+  end
+
+  def new
+    value = params.require(:category).permit(:name, :favorite)
+    category = Category.new value
+    
+    if category.save
+      redirect_to categories_url
+    else
+      redirect_to categories_url
+    end
+  end
+
+end
