6c2e9d85d19cafe88200c594fe8e74386fadaf7a
Samuel Honorato <samuel.honorato.silva@gmail.com>
2019-02-01 13:33:19 -0300

    chave estrangeira de video em categoria e empty state de categoria

--- app/models/category.rb
+++ app/models/category.rb
@@ -4,4 +4,10 @@ class Category < ApplicationRecord
   validates :name, presence: true
   validates :name, length: { maximum: 16 }
 
+  before_destroy :destroy_all_videos
+
+  def destroy_all_videos
+    self.videos.destroy_all
+  end
+
 end
