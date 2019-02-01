6066e1bbb633bd30231458e211efce32f3644ac8
Samuel Honorato <samuel.honorato.silva@gmail.com>
2019-01-31 21:11:24 -0300

    Criacao de modelos de video e comentario

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_30_101904) do
+ActiveRecord::Schema.define(version: 2019_02_01_000357) do
 
   create_table "categories", force: :cascade do |t|
     t.string "name"
     t.boolean "favorite"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "comments", force: :cascade do |t|
+    t.string "author"
+    t.text "message"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
+
+  create_table "videos", force: :cascade do |t|
+    t.string "title"
+    t.string "url"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
