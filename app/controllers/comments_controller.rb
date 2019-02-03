947be5c8b7929667d7e7f329f6bea7eea00facc6
Samuel Honorato <samuel.honorato.silva@gmail.com>
2019-02-03 01:09:57 -0300

    redirecionando para video ao adicionar comentario

--- app/controllers/comments_controller.rb
+++ app/controllers/comments_controller.rb
@@ -1,11 +1,12 @@
 class CommentsController < ApplicationController
 
   def new
+    id = params[:video_id]
     value = params.require(:comment).permit(:author, :message)
-    value[:video_id] = params[:video_id]
+    value[:video_id] = id
 
     Comment.create value
-    redirect_to root_url
+    redirect_to controller: 'videos', action: 'show', id: id
   end
 
 end
