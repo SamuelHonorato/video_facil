46b1569bbe33f3451267af1846883c3c96c3e7a4
Samuel Honorato <samuel.honorato.silva@gmail.com>
2019-02-03 11:47:29 -0300

    validacao de presenca obrigatoria nos campos de video: titulo, url e id da categoria

--- app/models/video.rb
+++ app/models/video.rb
@@ -1,7 +1,10 @@
 class Video < ApplicationRecord
   belongs_to :category
   has_many :comments
-  
+
+  validates :category_id, presence: true
+  validates :title, presence: true
+  validates :url, presence: true
   validates :category_id, presence: true
 
 end
